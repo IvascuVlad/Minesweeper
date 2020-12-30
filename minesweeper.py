ed5e8ab19ab7cec0d8735d4fc96cfd4a50a29da5
IvascuVlad <61700616+IvascuVlad@users.noreply.github.com>
2020-12-30 17:04:11 +0200

    Remediere bug-uri si adaugare functionalitati
    Implementarea casutelor pentru jocuri cu numarul de casute, bombe si timp setate de utilizator,  adugarea animatiei de pierdere sau castig, remediere bug click inafara zonei de joc .

--- minesweeper.py
+++ minesweeper.py
@@ -10,6 +10,16 @@ game_over_flag = False
 desired_time_in_seconds = 64
 start_time_in_seconds = 0
 start_counting_flag = False
+lose_flag = False
+
+input_number_of_blocks_text = "blocks"
+input_number_of_blocks_flag = False
+input_number_of_minutes_text = "minutes"
+input_number_of_minutes_flag = False
+input_number_of_bombs_text = "bombs"
+input_number_of_bombs_flag = False
+input_number_of_seconds_text = "seconds"
+input_number_of_seconds_flag = False
 
 hidden_matrix = []
 visible_matrix = []
@@ -84,7 +94,9 @@ five = pygame.image.load("REALE/5.png")
 bomb = pygame.image.load("bomb.png")
 flag = pygame.image.load("flag.png")
 question_mark = pygame.image.load("question.png")
-smiley_face = pygame.image.load("happy.png")
+smiley_face = pygame.image.load("win_and_happy.png")
+sad_face = pygame.image.load("sad.png")
+happy_face = pygame.image.load("happy.png")
 
 def drawGrid():
     top_left = (10, (window_height - window_width) + 10)
@@ -156,41 +168,144 @@ def drawBoxesInit():
             elif visible_matrix[x][y] != "D":
                 window.blit(block, (y * blockSize + top_left[0], x * blockSize + top_left[1]))
 
-def process_click(position, button):
+def process_click(event):
     global game_over_flag
     global start_counting_flag
     global start_time_in_seconds
+    global input_number_of_blocks_flag
+    global input_number_of_bombs_flag
+    global input_number_of_minutes_flag
+    global input_number_of_seconds_flag
+    global input_number_of_blocks_text
+    global input_number_of_bombs_text
+    global input_number_of_minutes_text
+    global input_number_of_seconds_text
+    global lose_flag
     top_left = (10, (window_height - window_width) + 10)
     top_right = (window_width - 10, (window_height - window_width) + 10)
+    bottom_right = (window_width - 10, window_height - 10)
     blockSize = int((top_right[0] - top_left[0]) / number_of_blocks)
-    y_position = int((position[0] - top_left[0]) / blockSize)
-    x_position = int((position[1] - top_left[1]) / blockSize)
+    y_position = int((event.pos[0] - top_left[0]) / blockSize)
+    x_position = int((event.pos[1] - top_left[1]) / blockSize)
     if not game_over_flag:
-        if visible_matrix[x_position][y_position] == None and button == 1:
-            if hidden_matrix[x_position][y_position] != "B":
-                if hidden_matrix[x_position][y_position] == 0:
-                    find_empty_neighbours(x_position, y_position)
-                visible_matrix[x_position][y_position] = "D"
-                if not start_counting_flag:
-                    start_time_in_seconds = int(pygame.time.get_ticks() / 1000)
-                start_counting_flag = True
-            else:
-                print("AI PIERDUT")
-                game_over_flag = True
-        if button == 3:
-            if visible_matrix[x_position][y_position] == None:
-                if not start_counting_flag:
-                    start_time_in_seconds = int(pygame.time.get_ticks() / 1000)
-                start_counting_flag = True
-                visible_matrix[x_position][y_position] = "F"
-            elif visible_matrix[x_position][y_position] == "F":
-                visible_matrix[x_position][y_position] = "?"
-            elif visible_matrix[x_position][y_position] == "?":
-                visible_matrix[x_position][y_position] = None
-    game_reset_x_coordinate = (10, window_width - 10)
-    game_reset_y_coordinate = (10, window_height - window_width - 10)
-    if button == 1 and game_reset_x_coordinate[0] < position[0] and position[0] < game_reset_x_coordinate[1] and game_reset_y_coordinate[0] < position[1] and position[1] < game_reset_y_coordinate[1]:
+        if top_left[0] <= event.pos[0] and event.pos[0] <= bottom_right[0] and top_left[1] <= event.pos[1] and event.pos[1] <= bottom_right[1]:
+            if visible_matrix[x_position][y_position] == None and event.button == 1:
+                if hidden_matrix[x_position][y_position] != "B":
+                    if hidden_matrix[x_position][y_position] == 0:
+                        find_empty_neighbours(x_position, y_position)
+                    visible_matrix[x_position][y_position] = "D"
+                    if not start_counting_flag:
+                        start_time_in_seconds = int(pygame.time.get_ticks() / 1000)
+                    start_counting_flag = True
+                else:
+                    print("AI PIERDUT")
+                    lose_flag = True
+                    game_over_flag = True
+            if event.button == 3:
+                if visible_matrix[x_position][y_position] == None:
+                    if not start_counting_flag:
+                        start_time_in_seconds = int(pygame.time.get_ticks() / 1000)
+                    start_counting_flag = True
+                    visible_matrix[x_position][y_position] = "F"
+                elif visible_matrix[x_position][y_position] == "F":
+                    visible_matrix[x_position][y_position] = "?"
+                elif visible_matrix[x_position][y_position] == "?":
+                    visible_matrix[x_position][y_position] = None
+    control_panel_x_coordinate = (10, window_width - 10)
+    control_panel_y_coordinate = (10, window_height - window_width - 10)
+    game_reset_x_coordinate = (control_panel_x_coordinate[1] - control_panel_x_coordinate[0]) / 2 - 10
+    game_reset_y_coordinate = (control_panel_y_coordinate[1] - control_panel_y_coordinate[0]) / 2 - 10
+    if event.button == 1 and game_reset_x_coordinate < event.pos[0] and event.pos[0] < game_reset_x_coordinate + 40 and game_reset_y_coordinate < event.pos[1] and event.pos[1] < game_reset_y_coordinate + 40:
         reset_game()
+    input_number_of_blocks_coord = (10 + control_panel_x_coordinate[0], 10 + control_panel_y_coordinate[0],
+                                    10 + control_panel_x_coordinate[0] + game_reset_x_coordinate / 2 - 25,
+                                    10 + control_panel_y_coordinate[0] + game_reset_y_coordinate - 5)
+    if event.button == 1 and input_number_of_blocks_coord[0] <= event.pos[0] and event.pos[0] <= \
+            input_number_of_blocks_coord[2] and input_number_of_blocks_coord[1] <= event.pos[1] and event.pos[1] <= \
+            input_number_of_blocks_coord[3]:
+        print("input_number_of_blocks")
+        input_number_of_blocks_text = ""
+        input_number_of_blocks_flag = True
+        input_number_of_bombs_flag = False
+        input_number_of_minutes_flag = False
+        input_number_of_seconds_flag = False
+
+    input_number_of_bombs_coord = (control_panel_x_coordinate[0] + game_reset_x_coordinate / 2 - 5, 10 + control_panel_y_coordinate[0],control_panel_x_coordinate[0] + game_reset_x_coordinate - 30, 10 + control_panel_y_coordinate[0] + game_reset_y_coordinate - 5)
+    if event.button == 1 and input_number_of_bombs_coord[0] <= event.pos[0] and event.pos[0] <= \
+            input_number_of_bombs_coord[2] and input_number_of_bombs_coord[1] <= event.pos[1] and event.pos[1] <= \
+            input_number_of_bombs_coord[3]:
+        print("input_number_of_bombs")
+        input_number_of_bombs_text = ""
+        input_number_of_blocks_flag = False
+        input_number_of_bombs_flag = True
+        input_number_of_minutes_flag = False
+        input_number_of_seconds_flag = False
+
+    input_number_of_minutes_coord = (10 + control_panel_x_coordinate[0], 10 + control_panel_y_coordinate[0] + game_reset_y_coordinate + 5 ,10 + control_panel_x_coordinate[0] + game_reset_x_coordinate / 2 - 25,10 + control_panel_y_coordinate[0] + 2 * game_reset_y_coordinate)
+    if event.button == 1 and input_number_of_minutes_coord[0] <= event.pos[0] and event.pos[0] <= \
+            input_number_of_minutes_coord[2] and input_number_of_minutes_coord[1] <= event.pos[1] and event.pos[1] <= \
+            input_number_of_minutes_coord[3]:
+        print("input_number_of_minutes")
+        input_number_of_minutes_text = ""
+        input_number_of_blocks_flag = False
+        input_number_of_bombs_flag = False
+        input_number_of_minutes_flag = True
+        input_number_of_seconds_flag = False
+
+    input_number_of_seconds_coord = (control_panel_x_coordinate[0] + game_reset_x_coordinate / 2 - 5,10 + control_panel_y_coordinate[0] + game_reset_y_coordinate + 5, control_panel_x_coordinate[0] + game_reset_x_coordinate -30 , 10 + control_panel_y_coordinate[0] + 2 * game_reset_y_coordinate)
+    if event.button == 1 and input_number_of_seconds_coord[0] <= event.pos[0] and event.pos[0] <= \
+            input_number_of_seconds_coord[2] and input_number_of_seconds_coord[1] <= event.pos[1] and event.pos[1] <= \
+            input_number_of_seconds_coord[3]:
+        print("input_number_of_seconds")
+        input_number_of_seconds_text = ""
+        input_number_of_blocks_flag = False
+        input_number_of_bombs_flag = False
+        input_number_of_minutes_flag = False
+        input_number_of_seconds_flag = True
+
+def process_key(event):
+    global input_number_of_blocks_text
+    global input_number_of_blocks_flag
+    global input_number_of_bombs_text
+    global input_number_of_bombs_flag
+    global input_number_of_minutes_text
+    global input_number_of_minutes_flag
+    global input_number_of_seconds_text
+    global input_number_of_seconds_flag
+    global number_of_blocks
+    global number_of_bombs
+    global desired_time_in_seconds
+    if input_number_of_blocks_flag:
+        if event.key == pygame.K_RETURN:
+            print(input_number_of_blocks_text)
+        elif event.key == pygame.K_BACKSPACE:
+            input_number_of_blocks_text = input_number_of_blocks_text[:-1]
+        else:
+            input_number_of_blocks_text += event.unicode
+
+    if input_number_of_bombs_flag:
+        if event.key == pygame.K_RETURN:
+            print(input_number_of_bombs_text)
+        elif event.key == pygame.K_BACKSPACE:
+            input_number_of_bombs_text = input_number_of_bombs_text[:-1]
+        else:
+            input_number_of_bombs_text += event.unicode
+
+    if input_number_of_minutes_flag:
+        if event.key == pygame.K_RETURN:
+            print(input_number_of_minutes_text)
+        elif event.key == pygame.K_BACKSPACE:
+            input_number_of_minutes_text = input_number_of_minutes_text[:-1]
+        else:
+            input_number_of_minutes_text += event.unicode
+
+    if input_number_of_seconds_flag:
+        if event.key == pygame.K_RETURN:
+            print(input_number_of_seconds_text)
+        elif event.key == pygame.K_BACKSPACE:
+            input_number_of_seconds_text = input_number_of_seconds_text[:-1]
+        else:
+            input_number_of_seconds_text += event.unicode
 
 def find_empty_neighbours(x, y):
     nextx = [-1, -1, -1, 0, 0, 1, 1, 1]
@@ -230,19 +345,42 @@ def reveal_all_bombs():
                 visible_matrix[i][j] = "D"
 
 def draw_control_panel():
+    global input_number_of_blocks_text
+    global input_number_of_bombs_text
+    global input_number_of_minutes_text
+    global input_number_of_seconds_text
+    global lose_flag
+    global game_over_flag
     control_panel_x_coordinate = (10, window_width - 10)
     control_panel_y_coordinate = (10, window_height - window_width - 10)
-    game_reset = pygame.transform.scale(smiley_face,(40,40))
+    game_reset = pygame.transform.scale(smiley_face, (40, 40))
+    if lose_flag:
+        game_reset = pygame.transform.scale(sad_face, (40, 40))
+    if game_over_flag and not lose_flag:
+        game_reset = pygame.transform.scale(happy_face, (40, 40))
     game_reset_x_coordinate = (control_panel_x_coordinate[1] - control_panel_x_coordinate[0]) / 2 - 10
     game_reset_y_coordinate = (control_panel_y_coordinate[1] - control_panel_y_coordinate[0]) / 2 - 10
     window.blit(game_reset,(game_reset_x_coordinate,game_reset_y_coordinate))
+
     input_number_of_blocks = pygame.Rect(10 + control_panel_x_coordinate[0], 10 + control_panel_y_coordinate[0], game_reset_x_coordinate / 2 - 25, game_reset_y_coordinate - 5)
+    font = pygame.font.SysFont('Consolas', int(game_reset_y_coordinate - 15))
+    txt_surface_blocks = font.render(input_number_of_blocks_text, True, (0,0,0))
+    window.blit(txt_surface_blocks, (input_number_of_blocks.x + 5, input_number_of_blocks.y + 5))
     pygame.draw.rect(window, (0,0,0), input_number_of_blocks, 2)
+
     input_number_of_minutes = pygame.Rect(10 + control_panel_x_coordinate[0], 10 + control_panel_y_coordinate[0] + game_reset_y_coordinate + 5 ,game_reset_x_coordinate / 2 - 25, game_reset_y_coordinate - 5)
+    txt_surface_minutes = font.render(input_number_of_minutes_text, True, (0, 0, 0))
+    window.blit(txt_surface_minutes, (input_number_of_minutes.x + 5, input_number_of_minutes.y + 5))
     pygame.draw.rect(window, (0, 0, 0), input_number_of_minutes, 2)
+
     input_number_of_bombs = pygame.Rect(control_panel_x_coordinate[0] + game_reset_x_coordinate / 2 - 5, 10 + control_panel_y_coordinate[0],game_reset_x_coordinate / 2 - 25, game_reset_y_coordinate - 5)
+    txt_surface_bombs = font.render(input_number_of_bombs_text, True, (0, 0, 0))
+    window.blit(txt_surface_bombs, (input_number_of_bombs.x + 5, input_number_of_bombs.y + 5))
     pygame.draw.rect(window, (0, 0, 0), input_number_of_bombs, 2)
+
     input_number_of_seconds = pygame.Rect(control_panel_x_coordinate[0] + game_reset_x_coordinate / 2 - 5,10 + control_panel_y_coordinate[0] + game_reset_y_coordinate + 5,game_reset_x_coordinate / 2 - 25, game_reset_y_coordinate - 5)
+    txt_surface_seconds = font.render(input_number_of_seconds_text, True, (0, 0, 0))
+    window.blit(txt_surface_seconds, (input_number_of_seconds.x + 5, input_number_of_seconds.y + 5))
     pygame.draw.rect(window, (0, 0, 0), input_number_of_seconds, 2)
 
 def draw_time():
@@ -276,17 +414,46 @@ def reset_game():
     global hidden_matrix
     global visible_matrix
     global number_of_blocks
+    global number_of_bombs
+    global desired_time_in_seconds
     global game_over_flag
     global start_time_in_seconds
     global start_counting_flag
+    global input_number_of_blocks_text
+    global input_number_of_blocks_flag
+    global input_number_of_minutes_text
+    global input_number_of_minutes_flag
+    global input_number_of_bombs_text
+    global input_number_of_bombs_flag
+    global input_number_of_seconds_text
+    global input_number_of_seconds_flag
+    global lose_flag
+    if input_number_of_bombs_text != "bombs":
+        number_of_bombs = int(input_number_of_bombs_text)
+    if input_number_of_blocks_text != "blocks":
+        number_of_blocks = int(input_number_of_blocks_text)
+    if input_number_of_seconds_text != "seconds" or input_number_of_minutes_text != "minutes":
+        desired_time_in_seconds = 0
+        if input_number_of_seconds_text != "seconds":
+            desired_time_in_seconds += int(input_number_of_seconds_text)
+        if input_number_of_minutes_text != "minutes":
+            desired_time_in_seconds += int(input_number_of_minutes_text) * 60
     hidden_matrix = []
     visible_matrix = []
-    number_of_blocks = 9
     generate_matrix()
     generate_matrix_game()
     game_over_flag = False
     start_time_in_seconds = 0
     start_counting_flag = False
+    #input_number_of_blocks_text = "blocks"
+    input_number_of_blocks_flag = False
+    #input_number_of_minutes_text = "minutes"
+    input_number_of_minutes_flag = False
+    #input_number_of_bombs_text = "bombs"
+    input_number_of_bombs_flag = False
+    #input_number_of_seconds_text = "seconds"
+    input_number_of_seconds_flag = False
+    lose_flag = False
 
 running = True
 
@@ -299,7 +466,9 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            process_click(event.pos, event.button)
+            process_click(event)
+        elif event.type == pygame.KEYDOWN:
+            process_key(event)
 
     window.fill((192,192,192))
     draw_delimiters()
